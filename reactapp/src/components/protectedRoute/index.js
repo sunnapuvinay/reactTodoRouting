e039afad264dc23c981aa6a8ef35cca6f539ceac
sunnapuvinay <138471157+sunnapuvinay@users.noreply.github.com>
2023-07-06 15:26:19 +0530

    Update index.js

--- reactapp/src/components/protectedRoute/index.js
+++ reactapp/src/components/protectedRoute/index.js
@@ -1,8 +1,6 @@
 import { Navigate} from 'react-router-dom';
 import Cookie from 'js-cookie';
 
-
-
 const ProtectedRoute = ({children}) => {
   const token = Cookie.get('jwtToken')
   if (token === undefined) {
